0c9741d9b015f9b6ef4e8a2765ab4606a27e4d02
sumkrishna <sumanth.krishna@avenues.org>
2024-11-05 10:31:55 -0300

    Update dashboard.tsx

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -127,7 +127,8 @@ const prepareChartData = (data: EconomicData[], removeOutliers: boolean = false)
           item["IPCA Inflation Rate (% Annual Variation)"] !== undefined ||
           item["Minimum Wage Growth Rate percentage"] !== undefined ||
           item["Growth rate from a Real GDP per capita"] !== undefined ||
-          item["Trade Balance Surplus Growth Rate"] !== undefined
+          item["Trade Balance Surplus Growth Rate"] !== undefined ||
+          item["Exchange Rate (January,/R$USD) growth"] !== undefined
         )
       );
     })
